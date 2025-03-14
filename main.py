f8ac90dfd08c75baa0bc308a2ac97d924369d888
Yang Xin Rong <99471368+xr93@users.noreply.github.com>
2025-03-14 15:14:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
 import plotly.express as px
-
+import seaborn as sns
 # Set the title of the main page
 st.title("Welcome to the Covid-19 Data App")
 
